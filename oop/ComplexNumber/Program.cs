6a8d2baf492a15f60dc8ed1ea18ac0f0bbcd37eb
Aashutosh <aashutoshtaikar@gmail.com>
2019-05-10 17:43:15 -0700

    Update Program.cs

--- oop/ComplexNumber/Program.cs
+++ oop/ComplexNumber/Program.cs
@@ -12,16 +12,19 @@ namespace ConsoleApp1
          
         static void Main(string[] args)
         {
-            cn x = new cn(1, 2);
-            cn y = new cn(3, 4);
+            cn x = new cn(1, 2); //this is a complex number 1+2i
+            cn y = new cn(3, 4); //3+4i
 
-            cn a = x - y;
+            cn somecom = new cn(1, 2) + new cn(3, 4);
+            somecom.Print();
+
+            cn a = x - y; //(1+2i) - (3+4i)
             a.Print();
 
-            a = new cn(1, 2) * new cn(3, 4);
+            a = new cn(1, 2) * new cn(3, 4); //(1 + 2i) *(3 + 4i)
             a.Print();
 
-            a = x / y;
+            a = x / y; //(1+2i) / (3+4i) 
             a.Print();
 
             Console.Read();
